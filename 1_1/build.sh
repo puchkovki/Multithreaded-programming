ccad89175d8fead2cda03e64e70cde461a4920cf
puchkovki <puchkov.k@phystech.edu>
2020-05-03 22:31:13 +0300

    Updated README

---
+++ 1_1/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FULLDIR=$(dirname "$0")
+g++ -Wall -Wextra -pedantic -O3 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion \
+-Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
+-fsanitize=address,undefined -fno-sanitize-recover=all -fstack-protector \
+-std=c++17 -pthread -o $FULLDIR/test $FULLDIR/thread_c++.cpp || exit 1
+$FULLDIR/test $1
+exit $?
--- 1_seminar/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FULLDIR=$(dirname "$0")
-g++ -Wall -Wextra -pedantic -O3 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address,undefined -fno-sanitize-recover=all -fstack-protector -std=c++17 -pthread -o $FULLDIR/test $FULLDIR/thread_c++.cpp || exit 1
-$FULLDIR/test $1
-exit $?
